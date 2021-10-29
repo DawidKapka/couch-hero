05c4c604fcc02eda09e33bf2e2cf65d4c2789653
DawidKapka <kapkadawid6@gmail.com>
2021-10-29 17:41:28 +0200

    add pages and routing

---
+++ couch-hero-frontend/src/app/pages/active-time/active-time.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-time',
+  templateUrl: './active-time.component.html',
+  styleUrls: ['./active-time.component.scss'],
+})
+export class ActiveTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ couch-hero-frontend/src/app/pages/bed-time/bed-time.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bed-time',
+  templateUrl: './bed-time.component.html',
+  styleUrls: ['./bed-time.component.scss'],
+})
+export class BedTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
