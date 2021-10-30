5dca568b16a020e8859c71655565a30bae25f2bf
leonieroth25 <leonieroth25@gmail.com>
2021-10-30 14:31:12 +0200

    finish notification page

--- couch-hero-frontend/src/app/pages/notifications/notifications.component.ts
+++ couch-hero-frontend/src/app/pages/notifications/notifications.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {User} from "../../shared/models/user.model";
 
 @Component({
   selector: 'app-notifications',
@@ -6,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
-
+  friend: User;
   isFirstChoosen: boolean = true;
   notificationTypes= ['Status', 'Friends', 'Groups'];
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.friend = userService.getFriend();
+  }
 
   toggleFirstChoosen(e) {
     this.isFirstChoosen = e;
